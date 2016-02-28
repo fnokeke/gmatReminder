140681104c1c3959cc930de7e335e8d9b2a50c70
Fabian Okeke <fnokeke@gmail.com>
2016-02-28 03:13:03 -0500

    alarm and notification working with multiple button samples

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,16 +1,24 @@
 angular.module('starter.controllers', [])
 
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+      console.log("ionic keyboard or statusBar done");
+    });
+  })
+
+
   .controller('DashCtrl', function ($scope) {
   })
 
-  .controller('ChatsCtrl', function ($scope, DataServiceHTTP, Chats) {
-    // With the new view caching in Ionic, Controllers are only called
-    // when they are recreated or on app start, instead of every page change.
-    // To listen for when this page is active (for example, to refresh data),
-    // listen for the $ionicView.enter event:
-    //
-    //$scope.$on('$ionicView.enter', function(e) {
-    //});
+  .controller('ChatsCtrl', function ($scope, DataServiceHTTP, $cordovaLocalNotification, $ionicPlatform, Chats) {
 
     $scope.items = [];
     $scope.reminder = {
@@ -45,14 +53,6 @@ angular.module('starter.controllers', [])
       $scope.reminder.saveDisabled = true;
     };
 
-    $scope.startCountdown = function () {
-      console.log("countdown started");
-    };
-
-    $scope.showNotification = function () {
-      console.log("notification popup");
-    };
-
     $scope.testNutrition = function () {
       console.log("testing nutrition.");
 
@@ -73,6 +73,90 @@ angular.module('starter.controllers', [])
     $scope.remove = function (chat) {
       Chats.remove(chat);
     };
+
+    $ionicPlatform.ready(function () {
+
+      $scope.scheduleSingleNotification = function () {
+        $cordovaLocalNotification.schedule({
+          id: 1,
+          title: 'GMAT Practice Reminder',
+          text: 'Start practicing within next 10 mins to receive your reward.',
+          //data: {
+          //  customProperty: 'custom value'
+          //}
+        }).then(function (result) {
+          console.log('Notification 1 triggered');
+        }).catch(
+          function (reason) {
+            console.log('Rejection error: (' + reason + ').');
+          });
+      };
+
+      $scope.scheduleDelayedNotification = function () {
+        var now = new Date().getTime();
+        var _10SecondsFromNow = new Date(now + 10 * 1000);
+
+        $cordovaLocalNotification.schedule({
+          id: 2,
+          title: 'Warning',
+          text: 'Im so late',
+          at: _10SecondsFromNow
+        }).then(function (result) {
+          console.log('Notification 2 triggered');
+        });
+      };
+
+      $scope.scheduleEveryMinuteNotification = function () {
+        $cordovaLocalNotification.schedule({
+          id: 3,
+          title: 'Warning',
+          text: 'Dont fall asleep',
+          every: 'minute'
+        }).then(function (result) {
+          console.log('Notification 3 triggered');
+        });
+      };
+
+      $scope.scheduleGMATNotification = function () {
+        var one_hour = 3600 * 1000;
+        var one_minute = 60 * 1000;
+        var now = new Date().getTime(),
+          _hrs_from_now = new Date(now + 6 * one_hour),
+          _mins_from_now = new Date(now + 3 * one_minute);
+        $cordovaLocalNotification.schedule({
+          id: 10,
+          title: 'GMAT Gentle Reminder',
+          text: 'Time to practice (start within next 10 minutes)',
+          every: 'day',
+          firstAt: _hrs_from_now
+        }).then(function (result) {
+          console.log('Notification 3 triggered');
+        });
+      };
+
+      $scope.cancelGMATNotification = function () {
+        $cordovaLocalNotification.cancel(10).then(function (result) {
+          console.log('Notification 10 Canceled');
+        });
+      };
+
+      $scope.updateSingleNotification = function () {
+        $cordovaLocalNotification.update({
+          id: 2,
+          title: 'Warning Update',
+          text: 'This is updated text!'
+        }).then(function (result) {
+          console.log('Notification 1 Updated');
+        });
+      };
+
+      $scope.cancelSingleNotification = function () {
+        $cordovaLocalNotification.cancel(3).then(function (result) {
+          console.log('Notification 3 Canceled');
+        });
+      };
+
+    });
 
   })
 
