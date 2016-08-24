8b42a7ae139dac499ceeb8c07d2e33e75cedc92d
Fabian Okeke <fnokeke@gmail.com>
2016-08-23 20:52:54 -0400

    now using only 3 major files: one for all user profiles, one for all user practices, and one for all user reminders

--- data/stats_gmat.py
+++ data/stats_gmat.py
@@ -1,10 +1,14 @@
-# get stats of all users on the dashboard
+"""
+filename: stats_gmat.py
+description: get stats of all users on the dashboard
+"""
 
-import requests
 import json
+import requests
 
 
 def fetch_data(page):
+    """ REST request to gmat api to fetch paginated info for all users """
     url = 'http://slm.smalldata.io/gmat/api/student?page={0}'.format(page)
     r = requests.get(url)
     if r.status_code != 200:
@@ -19,65 +23,128 @@ def fetch_data(page):
 
 # counter for keeping track of files
 # there should be 120 files in total
-class Counter(object):
+class CSV_FILE(object):
+    """ simple class to just keep count of number of users' account """
     count = 0
     limit = 124
 
 
-# store each user's data in a separate file
-def write_to_file(data):
+# three files: (1)all user profiles (2)all practices (3)all reminders
+def store_data(data, filenames):
+    """ extract users from data object and store the info in 3 files """
     if not data:
         return
 
     users = data['objects']
-    print 'user len:', len(users)
+    print 'No of users: {}'.format(len(users))
+    print
 
-    for user in users[:3]:
-        Counter.count += 1
-        assert Counter.count <= Counter.limit, 'Counter should be <= limit'
+    for i, user in enumerate(users):
+        profile, practices, reminders = get_info(user)
+        if not (practices and reminders):
+            continue
 
-        filename = 'stats/{0}.csv'.format(Counter.count)
-        header = 'code, student_id, email, no_of_practices, has_contingency, has_deadline'
-        content = extract_user_content(Counter.count, header, user)
+        store_profile(profile, filenames['profiles'])
+        store_practices(practices, filenames['practices'])
+        store_reminders(reminders, filenames['reminders'])
 
-        with open(filename, 'w') as f:
-            f.write(header)
-            f.write('\n')
-            f.write(content)
+        print 'user{} info stored...'.format(i)
 
 
-def extract_user_content(i, header, user):
-    code = user['code']
-    has_contingency = user['has_contingency']
-    has_deadline = user['has_deadline']
-
+def get_info(user):
+    """ obtain three categories of info for given user """
     practices = user['practices']
-    no_of_practices = len(practices)
+    reminders = user['reminders']
+    no_of_practices, no_of_reminders = len(practices), len(reminders)
 
-    account = user['account']
-    email = account['email']
-    student_id = account['student_id']
+    account, code = user['account'], user['code']
+    has_contingency, has_deadline = user['has_contingency'], user['has_deadline']
+    email, student_id = account['email'], account['student_id']
 
-    key = ['{' + x + '}' for x in header.split(', ')]
-    key = ','.join(key)
-    fmt = key.format(  # fmt = '{code}, {student_id}, {email} ...
+    key = '{code}, {student_id}, {email}, {no_of_practices}, \
+           {no_of_reminders}, {has_contingency}, {has_deadline}'
+
+    profile = key.format(
         code=code,
         student_id=student_id,
         email=email,
         no_of_practices=no_of_practices,
+        no_of_reminders=no_of_reminders,
         has_contingency=has_contingency,
         has_deadline=has_deadline)
 
-    return fmt
+    return profile, practices, reminders  # str, list, list
+
+
+def store_profile(profile, filename):
+    """ append all users' profiles in one file """
+    assert CSV_FILE.count <= CSV_FILE.limit, 'CSV_FILE should be <= limit'
+
+    with open(filename, 'a') as f:
+        f.write(profile)
+        f.write('\n')
+
+    CSV_FILE.count += 1
+
+
+def store_practices(practices, filename):
+    """ append all users' practices in single file """
+
+    if not practices:
+        with open(filename, 'a') as f:
+            f.write('No practice sessions')
+            f.write('\n')
+
+    with open(filename, 'a') as f:
+        for practice in practices:
+            student_id = practice['student_id']
+            question_count = practice['question_count']
+            percent_correct = practice['percent_correct']
+            duration = practice['duration']
+            taken_on = practice['taken_on']
+            # reminder_when_taken = practice['reminder_when_taken']['remind_time']
+
+            li = [student_id, taken_on, question_count, percent_correct, duration, '\n']
+            li = [str(x) for x in li]
+            content = ','.join(li)
+            f.write(content)
+
+
+def store_reminders(reminders, filename):
+    """ append all users' reminders in one file """
+
+    if not reminders:
+        with open(filename, 'a') as f:
+            f.write('No reminder sessions')
+            f.write('\n')
+
+    with open(filename, 'a') as f:
+        for reminder in reminders:
+            student_id = reminder['student_id']
+            remind_time = reminder['remind_time']
+            created_at = reminder['created_at']
+
+            li = [student_id, remind_time, created_at, '\n']
+            li = [str(x) for x in li]
+            content = ','.join(li)
+            f.write(content)
 
 #########################################
-################ main ###################
+# main
 #########################################
 if __name__ == '__main__':
     print 'Downloading stats info...'
 
-    for page in range(7, 8):
-        data = fetch_data(page)
-        write_to_file(data)
+    stores = {
+        'profiles': 'profile.csv',
+        'reminders': 'reminders.csv',
+        'practices': 'practices.csv',
+    }
+
+    start, end = 1, 14
+    for page_num in range(start, end):
+        print 'Fetching page{} data...'.format(page_num)
+        details = fetch_data(page_num)
+        store_data(details, stores)
 
     print 'Done!'
